6f7ff0e6981ffc78f24e0a57d33af8f1ccfd4fdc
Leonard Okyere Afeke <leo.afeke@outlook.com>
2024-10-21 12:59:04 -0400

    Started project developers

---
+++ devs/developers/users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def profiles(request):
+    
+    
+    context = {}
+    return render(request, 'users/profiles.html', context)
+
+def user_profile(request, pk):
+    
+    
+    context = {}
+    return render(request, 'users/user-profile.html', context)
